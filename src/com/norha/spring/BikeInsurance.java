4758a335aa6bacce9be2c81e6f990b00f5c29c8a
Robinclear <robin.peter.vkm@gmail.com>
2020-04-16 17:32:14 -0300

    Dependency is injected by the use of Beans.xml file as constructor-arg

--- src/com/norha/spring/BikeInsurance.java
+++ src/com/norha/spring/BikeInsurance.java
@@ -3,12 +3,6 @@ package com.norha.spring;
 public class BikeInsurance implements Insurance {
 	private Offers offers;
 
-	/*
-	 * This Default constructor is must during the bean 
-	 * In the Bean we can pass the Offers class object as argument but
-	 * in case we are not passing it default constructor will called 
-	 * at the time of bean initialization
-	 */	
 	public BikeInsurance() {
 	}
 
@@ -18,6 +12,7 @@ public class BikeInsurance implements Insurance {
 
 	@Override
 	public String showStatus() {
+
 		System.out.println(offers.getOffer());
 		return "Your Bike is Insured";
 	}
