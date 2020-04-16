5ff76d373a46bcee3c4c6d0bb183547c87b2d2b5
Robinclear <robin.peter.vkm@gmail.com>
2020-04-16 17:58:29 -0300

    Dependency Injection (Property setter) ***********************************************
    <property name="offers" ref="off"></property>
    
    
    private Offers offers;
    
    Importent point
    ***************
    property name and the Member variable name should be matched

--- src/com/norha/spring/BikeInsurance.java
+++ src/com/norha/spring/BikeInsurance.java
@@ -2,17 +2,17 @@ package com.norha.spring;
 
 public class BikeInsurance implements Insurance {
 	private Offers offers;
-
-	public BikeInsurance() {
+	
+	public Offers getOffers() {
+		return offers;
 	}
 
-	public BikeInsurance(Offers o) {
+	public void setOffers(Offers o) {
 		this.offers = o;
 	}
 
 	@Override
 	public String showStatus() {
-
 		System.out.println(offers.getOffer());
 		return "Your Bike is Insured";
 	}
